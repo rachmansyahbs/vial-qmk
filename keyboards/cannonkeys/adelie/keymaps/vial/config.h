dc3b9d29c171c60047f97ec9a14575cac49d69da
Rachmansyah Budi Setiawan <rachmansyahbs@gmail.com>
2022-02-20 21:23:43 +0800

    VIAL keymap for Adelie

---
+++ keyboards/cannonkeys/adelie/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xA8, 0x35, 0x7F, 0x76, 0x86, 0x08, 0xF4, 0x44}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 1 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }

fe8e8e598e674ace2210ed0e6f7759be1726ab4d
Rachmansyah Budi Setiawan <rachmansyahbs@gmail.com>
2022-02-28 21:28:54 +0800

    Add VIAL keymap for 45ATS

---
+++ keyboards/45_ats/keymaps/vial/config.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x02, 0x8D, 0x4E, 0xDD, 0x88, 0xC5, 0x29, 0x61}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 1 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 12 }
+#define VIAL_TAP_DANCE_ENTRIES 2
+#define VIAL_COMBO_ENTRIES 2

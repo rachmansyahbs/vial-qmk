38fc8338e9a50c05459be8c4844e1e3ab37e60ef
Rachmansyah Budi Setiawan <rachmansyahbs@gmail.com>
2022-07-24 13:06:00 +0800

    Add vial support for Viktus SP Mini

---
+++ keyboards/viktus/sp_mini/keymaps/vial/config.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x37, 0x8E, 0x0A, 0x25, 0xD2, 0xBF, 0x6F, 0x03}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 7 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 6 }
+
+#define VIAL_COMBO_ENTRIES 2

264cd6ffb2ebc328cb6dd46c1381d00f0d92f299
langerhans <langerhans@users.noreply.github.com>
2022-08-20 17:24:15 +0200

    Add Calliope keyboard

---
+++ keyboards/calliope/keymaps/vial/config.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x9E, 0x01, 0x22, 0xE1, 0xA9, 0x5F, 0x4B, 0x55}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 2, 14 }
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 3

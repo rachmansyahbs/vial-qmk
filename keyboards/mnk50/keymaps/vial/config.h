eba9a6d7f90079aeb711492c15dea77727274e82
Rachmansyah Budi Setiawan <rachmansyahbs@gmail.com>
2022-12-18 14:55:06 +0800

    Add vial keymap for mnk50 (Monokei Neko)

---
+++ keyboards/mnk50/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x41, 0xF7, 0x64, 0xD9, 0xD5, 0x81, 0x58, 0x7D}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 1 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 12 }

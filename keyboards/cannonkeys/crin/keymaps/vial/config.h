2235964844b29562d5c98c6bd0fc599f3276a95c
Rachmansyah Budi Setiawan <rachmansyahbs@gmail.com>
2022-12-21 19:17:12 +0800

    Add vial keymap for Crin

---
+++ keyboards/cannonkeys/crin/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xC0, 0x83, 0x34, 0xCC, 0x3A, 0x75, 0x8B, 0xD5}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }

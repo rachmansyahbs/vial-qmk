4ace3946cffcb481f3984c1f3c3b21d3211e556a
Rachmansyah Budi Setiawan <rachmansyahbs@gmail.com>
2022-12-22 04:30:53 +0800

    Add vial keymap for Cloudline

---
+++ keyboards/cannonkeys/cloudline/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x60, 0xE9, 0x8B, 0x0B, 0x32, 0x91, 0x66, 0xF5}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }

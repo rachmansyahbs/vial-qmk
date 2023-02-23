383102063d5b6a563857fc2ca8ad1d86df188aff
Rachmansyah Budi Setiawan <rachmansyahbs@gmail.com>
2023-02-24 00:24:15 +0800

    add vial keymap for WT60-D

---
+++ keyboards/wilba_tech/wt60_d/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x2B, 0xF9, 0xCE, 0xC9, 0x01, 0x95, 0x3F, 0xD5}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }

f1b82039a10736a3908f3c6e90b710a49e93bd5d
Rachmansyah Budi Setiawan <rachmansyahbs@gmail.com>
2023-05-01 22:21:53 +0800

    Add vial keymap for kopibeng XT60 PCB
    - Some features disabled due to firmware size limit, including RGB light

---
+++ keyboards/kopibeng/xt60/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x0C, 0xC7, 0x55, 0x3E, 0xB1, 0x7F, 0x3C, 0x5A}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }

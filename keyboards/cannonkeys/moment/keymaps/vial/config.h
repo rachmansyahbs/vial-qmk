04098ada6613e5c996b2ab8aebbc72356c4c4be5
Rachmansyah Budi Setiawan <rachmansyahbs@gmail.com>
2023-11-04 16:28:34 +0800

    add vial keymap for moment

---
+++ keyboards/cannonkeys/moment/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x8B, 0xBF, 0xCC, 0x7E, 0xD6, 0x99, 0x55, 0xC8}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }

acb1a0f251d2f537ce2d45c405f5c6dd860f8aeb
Rachmansyah Budi Setiawan <rachmansyahbs@gmail.com>
2024-10-14 23:19:57 +0800

    add vial keymap for NK Classic TKL

---
+++ keyboards/novelkeys/nk_classic_tkl/keymaps/vial/config.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x9F, 0x6A, 0x10, 0x16, 0x8B, 0x6F, 0x2C, 0x11}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 3 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
+
+#define FEE_PAGE_COUNT 2
